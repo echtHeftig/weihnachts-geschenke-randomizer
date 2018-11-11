854542c1d40a93096402d088cdfe9bcbe8c2a326
mk <mk@example.com>
2018-11-11 11:01:46 +0100

    adds thirdDraft (Work in progress) - includes first working view

--- src/main/java/geschenke/view/MyMainView.java
+++ src/main/java/geschenke/view/MyMainView.java
@@ -6,6 +6,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import geschenke.model.Person;
 import geschenke.model.PresentTable;
 
 @SpringUI(path = "")
@@ -16,7 +17,13 @@ public class MyMainView extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        grid.setItems(new PresentTable());
+        grid.addColumn(aPresentTable -> aPresentTable.getSchenkender().getName())
+                .setCaption("Schenkende Person")
+                .setId("schenkendePersonId");
+        grid.addColumn(aPresentTable -> aPresentTable.getBeschenkter().getName())
+                .setCaption("Beschenkte Person")
+                .setId("beschenktePersonId");
+        grid.setColumns("schenkendePersonId", "beschenktePersonId");
         VerticalLayout verticalLayout = new VerticalLayout(grid, generateButton);
         generateButton.addClickListener(e -> updatePresentTable());
 
@@ -24,6 +31,9 @@ public class MyMainView extends UI {
     }
 
     private void updatePresentTable() {
-        System.out.println("mach mal nichts");
+        PresentTable items1 = new PresentTable(new Person("Egon"), new Person("Hans"));
+        PresentTable items2 = new PresentTable(new Person("Miau"), new Person("Hans"));
+        PresentTable items3 = new PresentTable(new Person("Timo"), new Person("Hanna"));
+        grid.setItems(items1, items3, items2);
     }
 }
