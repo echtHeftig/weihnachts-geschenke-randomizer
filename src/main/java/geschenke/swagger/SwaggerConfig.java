7cee8ab15a9cfd3362f8ecde37c49157bbacb227
mk <mk@example.com>
2019-02-23 12:59:33 +0100

    Deletes unused imports

--- src/main/java/geschenke/swagger/SwaggerConfig.java
+++ src/main/java/geschenke/swagger/SwaggerConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.Collections;
 
 
