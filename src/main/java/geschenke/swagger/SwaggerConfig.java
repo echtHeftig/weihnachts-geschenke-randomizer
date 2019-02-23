ed8da4b528d375902c6435b8457fd5092b48fea7
mk <mk@example.com>
2019-02-23 12:59:05 +0100

    Adds custom API info to Swagger Documentation

--- src/main/java/geschenke/swagger/SwaggerConfig.java
+++ src/main/java/geschenke/swagger/SwaggerConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +23,20 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("geschenke.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Geschenke - Api Documentation",
+                "Api Documentation for Geschenke an Weihnachten",
+                "0.0.1",
+                "urn:tos",
+                ApiInfo.DEFAULT_CONTACT,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList()
+        );
     }
 }
