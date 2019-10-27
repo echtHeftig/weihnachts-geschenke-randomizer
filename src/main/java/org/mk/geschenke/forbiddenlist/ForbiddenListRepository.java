ab5ded3d29976c67d05fd549a9ee7f727031c36c
mk <mk@example.com>
2019-10-27 12:28:36 +0100

    adds test that shows duplicate entry creation in forbiddenList repository

--- src/main/java/org/mk/geschenke/forbiddenlist/ForbiddenListRepository.java
+++ src/main/java/org/mk/geschenke/forbiddenlist/ForbiddenListRepository.java
@@ -1,7 +1,9 @@
 package org.mk.geschenke.forbiddenlist;
 
 import org.mk.geschenke.domain.ForbiddenList;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
-public interface ForbiddenListRepository extends CrudRepository<ForbiddenList, Long> {
+public interface ForbiddenListRepository extends JpaRepository<ForbiddenList, Long> {
 }
