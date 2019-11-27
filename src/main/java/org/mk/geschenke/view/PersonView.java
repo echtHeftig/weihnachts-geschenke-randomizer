0c4858d63b0eced37f0bc56ef97f3e4938e8cbf5
mk <mk@example.com>
2019-11-27 21:15:45 +0100

    adds delete feature to person view & refactoring

--- src/main/java/org/mk/geschenke/view/PersonView.java
+++ src/main/java/org/mk/geschenke/view/PersonView.java
@@ -1,6 +1,7 @@
 package org.mk.geschenke.view;
 
 import com.vaadin.data.Binder;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
@@ -8,14 +9,16 @@ import org.mk.geschenke.domain.Person;
 import org.mk.geschenke.person.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Set;
+
 @SpringUI(path = "personview")
 public class PersonView extends UI {
 
     private TextField textField = new TextField("Personen-Name");
     private Button button = new Button("Person hinzufügen");
+    private Button deleteButton = new Button("Markierte Person löschen", VaadinIcons.DEL);
     private Grid<Person> personGrid = new Grid<>(Person.class);
     private Binder<Person> binder = new Binder<>(Person.class);
-    private Person person;
 
     @Autowired
     private PersonService personService;
@@ -25,18 +28,27 @@ public class PersonView extends UI {
         binder.forField(textField)
                 .bind(Person::getName, Person::setName);
         button.addClickListener(e -> addPersonToGrid(new Person()));
+        deleteButton.addClickListener(c -> deletePersonFromGrid());
         personGrid.setItems(personService.getAllPersons());
         personGrid.setHeightByRows(12);
 
-        final VerticalLayout verticalLayout = new VerticalLayout(new NavigationHorizontalLayout().getNavigationLayout(), textField, button, personGrid);
+        final VerticalLayout verticalLayout = new VerticalLayout(new NavigationHorizontalLayout().getNavigationLayout(), textField, button, personGrid, deleteButton);
 
         setContent(verticalLayout);
     }
 
     private void addPersonToGrid(Person person) {
-        this.person = person;
-        binder.writeBeanIfValid(this.person);
-        personService.addPerson(this.person);
+        binder.writeBeanIfValid(person);
+        personService.addPerson(person);
+        personGrid.setItems(personService.getAllPersons());
+    }
+
+    private void deletePersonFromGrid() {
+        Set<Person> selectedItems = personGrid.getSelectedItems();
+        //TODO: Fix Bug in ForbiddenView / Service / Repo -> If Person is deleted then all forbiddenPairs containing this name have to be removed
+        for (Person person : selectedItems) {
+            personService.deletePersonByName(person.getName());
+        }
         personGrid.setItems(personService.getAllPersons());
     }
 }
